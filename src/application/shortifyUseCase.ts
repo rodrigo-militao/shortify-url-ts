907c26c84592f802f660b1a0755dd605002bb541
Rodrigo Militão <militaorodrigo9@gmail.com>
2023-01-30 16:15:47 -0300

    refactor: generateNewDomain method instead new useCase

--- src/application/shortifyUseCase.ts
+++ src/application/shortifyUseCase.ts
@@ -1,8 +1,11 @@
+import Hashids from 'hashids';
 import { Url } from "../domain/url.entity";
 import { ShortifyRequest, validate } from "../domain/validations/shortifyValidaton";
-import { getByLongUrl } from "../infra/repository/urlRepository";
+import { getByLongUrl, save } from "../infra/repository/urlRepository";
 import { Result } from "../types";
-import newDomainUseCase from "./newDomainUseCase";
+
+const DEFAULT_URL = process.env.MYURL || "https://myUrl.com/";
+const DAYS_TO_EXPIRE = Number(process.env.DAYSTOEXPIRE) || 10;
 
 export default async (input: ShortifyRequest): Promise<Result<Url>> => {
   const validationResult = validate(input)
@@ -13,9 +16,31 @@ export default async (input: ShortifyRequest): Promise<Result<Url>> => {
   if (getByLongUrlResult.success)
     return { success: true, data: getByLongUrlResult.data }
 
-  const newDomainResult = await newDomainUseCase(input.longUrl);
+  const newDomainResult = await generateNewDomain(input.longUrl);
   if (newDomainResult.success)
     return {success: true, data: newDomainResult.data}
 
   return { success: false, error: newDomainResult.error }
+}
+
+const randomId = (): string => new Date().getTime().toString();
+const expirationDate = () => new Date(Date.now() + DAYS_TO_EXPIRE * 24*60*60*1000);
+
+const generateNewDomain = async (longUrl: string) : Promise<Result<Url>> => {
+  const hashids = new Hashids();
+  const uniqueId = randomId();
+  const encondedId = hashids.encode(uniqueId);
+
+  const url:Url = {
+    urlId: uniqueId,
+    shortUrl: DEFAULT_URL + encondedId,
+    expiresAt: expirationDate(),
+    longUrl
+  };
+
+  const resultSave = await save(url);
+
+  return resultSave 
+  ? {success: true, data: url}
+  : {success: false, error: "error while saving url"};
 }
